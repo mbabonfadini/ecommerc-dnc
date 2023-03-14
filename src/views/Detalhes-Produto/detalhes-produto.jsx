245b399b9852e9508e552ddf3d7908ff02407b02
Vanderlei Aniecevski <vanderlei@aniecevski.com.br>
2023-03-13 23:25:53 -0300

    fix:detalhes-produto

--- src/views/Detalhes-Produto/detalhes-produto.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const detalhes-produto = () => {
-  return (
-    <div>detalhes-produto</div>
-  )
-}
-
-export default detalhes-produto
---
+++ src/views/Detalhes-Produto/detalhesproduto.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const detalhesproduto = () => {
+  return <div>detalhes-produto</div>;
+};
+
+export default detalhesproduto;
