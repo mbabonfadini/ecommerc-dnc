6778fe8ff3a7933938898fbff66c1f74b87506bb
Robson Berghman <102102285+RobsonBerghman@users.noreply.github.com>
2023-03-20 20:49:26 -0300

    Correção do css e adição das validações
    Foi Corrigido a Centralizaão do botão entrar e adicionado as validações do inputs de login e senha .

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Header from '../../componentes/Header'
 
+
 const Login = () => {
+  
+  const [user, setUser]= useState("")
+
+  const[senha, setSenha]= useState("")
+
+  const navigate = useNavigate();
+
+  function Submit (event) {
+    event.preventDefault()
+    if(user===""|senha===""){alert("Preencher os campos corretamente!")}
+    else{navigate("/home")}
+} 
+  
   return (
     <section className='login'>
       <Header/>
-      <form className="login__form">
+      <form className="login__form" onSubmit={(evento)=> Submit(evento)} >
         <h1>Acesse com seu login ou cadastre-se!</h1>
         <h2>você pode entrar com o seu CPF</h2>
         <div className="login__input-wrapper">
           <label htmlFor="name">Digite seu CPF:</label>
           <input
             type="text"
-            placeholder="Nome completo"
+            placeholder="Nome completo" 
             id="name"
             className="login__input-name"
+            value={user} onChange={(evento)=> {setUser(evento.target.value)}} 
           />
           <label htmlFor="password">Senha:</label>
-          <input type="password" id="password" placeholder="********" />
+          <input type="password" id="password" placeholder="********" value={senha} onChange={(evento)=> {  console.log(senha)
+            setSenha(evento.target.value)}}/>
         </div>
-        <button type="submit">
-          <Link to={"/home"}>Entrar</Link>
+        <button type='submit'>
+          Entrar
         </button>
       </form>
     </section>
